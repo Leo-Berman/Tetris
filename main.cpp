141523d2d0166e1069131cb4c3778a760fb027eb
root <root@ENGR153.malvern.ezsoft-inc.com>
2024-06-19 15:05:06 -0400

    updates

--- main.cpp
+++ main.cpp
@@ -4,10 +4,21 @@ int main() {;
   Terminal terminal = Terminal();
   Game game = Game();
   int frame_counter = 0;
+
+  std::vector<std::thread> threads;
+  threads.push_back(std::thread(&Terminal::update_terminal,std::ref(terminal),game));
+
+  threads.push_back(std::thread(&Terminal::process_keystroke,std::ref(terminal),game));
+  for (auto &th : threads) {
+    th.join();
+  }
   
   while(true) {
 
-    //if(terminal.process_keystroke(game) == false) break;
+
+  
+  
+    if(terminal.process_keystroke(game) == false) break;
 
     terminal.update_terminal(game);
 
